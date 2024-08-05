069d8620c7e9120b3b96ee6be350ec94db807551
hibaasim <hiba.asim10@gmail.com>
2024-08-05 18:58:35 +0300

    task-3

---
+++ api/v1/app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" app for api """
+
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown_storage():
+    """ handles @app.teardown_appcontext """
+    storage.close()
+
+if __name__ == '__main__':
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(getenv('HBNB_API_PORT', 5000))
+    app.run(host=HOST, ports=PORT, threaded=True)
