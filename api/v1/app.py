270d3668c7ac7bb1f11f30dafb12a915e495c408
hibaasim <hiba.asim10@gmail.com>
2024-08-05 09:12:26 -0700

    task-3 fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,10 +9,12 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_storage(*args):
     """ handles @app.teardown_appcontext """
     storage.close()
+
 
 if __name__ == '__main__':
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status')
 def status():
     """routes a specific message for status"""
